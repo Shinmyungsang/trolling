e53a8c57d0561ebb8007d811dd456677ffc342ca
Shinmyungsang <mongddangand@naver.com>
2017-06-28 12:24:41 +0000

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+require 'open-uri'
+class PostsController < ApplicationController
+  def index
+    
+    @titles = Array.new
+    
+    15.downto(1) do |p|
+      doc = Nokogiri::HTML(open("http://www.inven.co.kr/board/powerbbs.php?come_idx=4677&query=list&my=&category=&category2=&sort=PID&orderby=&name=&subject=&content=&keyword=&sterm=&eq=&iskin=&mskin=&p=#{p}"))
+      
+      doc.css(".bbsSubject//a").each do |x|
+        @titles <<x.inner_text if x.inner_text.include?params[:trol]
+      end
+      
+    end
+  end
+end
