4157ecf4180cc5ebf01377ce45d4a35eb7fc3a5a
Shinmyungsang <mongddangand@naver.com>
2017-07-01 13:29:05 +0000

    second

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,8 @@ class PostsController < ApplicationController
       doc = Nokogiri::HTML(open("http://www.inven.co.kr/board/powerbbs.php?come_idx=4677&query=list&my=&category=&category2=&sort=PID&orderby=&name=&subject=&content=&keyword=&sterm=&eq=&iskin=&mskin=&p=#{p}"))
       
       doc.css(".bbsSubject//a").each do |x|
-        @titles <<x.inner_text if x.inner_text.include?params[:trol]
+        trol = params[:trol]
+        @titles <<x.inner_text if x.inner_text.include?("#{trol}")
       end
       
     end
