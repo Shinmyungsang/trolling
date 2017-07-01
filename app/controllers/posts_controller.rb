e535c3a98d881093f04f8c80ac314c30bdbf6d01
Shinmyungsang <mongddangand@naver.com>
2017-07-01 14:18:45 +0000

    3th

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
       doc = Nokogiri::HTML(open("http://www.inven.co.kr/board/powerbbs.php?come_idx=4677&query=list&my=&category=&category2=&sort=PID&orderby=&name=&subject=&content=&keyword=&sterm=&eq=&iskin=&mskin=&p=#{p}"))
       
       doc.css(".bbsSubject//a").each do |x|
-        trol = params[:trol]
+        trol = params[:trol] #이부분에 trol에 @를 붙히면 왜 작동을 안 할까..
         @titles <<x.inner_text if x.inner_text.include?("#{trol}")
       end
       
